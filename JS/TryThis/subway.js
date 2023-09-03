8738ddae84998e721c0f6aec4c0fc4d2577bb009
SNXWXH <snxwxh@gmail.com>
2023-09-03 23:03:28 +0900

    TryThis/subway.js

--- JS/TryThis/subway.js
+++ JS/TryThis/subway.js
@@ -64,7 +64,12 @@ class Subway {
 
   get isEnd() {
     return (
+      (this.startIdx === 0 &&
+        this.rotateCnt === 1 &&
+        this.currIdx === LINE2.length - 1) ||
+      //end가 없을 때
       (this.rotateCnt === 2 && this.currIdx === this.startIdx - 1) ||
+      //end가 있을 때
       (this.end && LINE2[this.currIdx] === this.end)
     );
   }
@@ -84,7 +89,6 @@ class Subway {
       if (this.isEnd) {
         break;
       }
-
       yield this.gotoNextStation();
       // yield `${
       //   this.rotateCnt * LINE2.length + this.currIdx - this.startIdx + 2
@@ -105,26 +109,28 @@ class Subway {
   }
 }
 
-// const routes = new Subway('신도림', '신림');
+// const routes = new Subway("신도림", "신림");
 // const routes = new Subway('문래', '신림');
-const routes = new Subway("문래");
+// const routes = new Subway("문래");
+const routes = new Subway("신도림");
+
 console.log([...routes]); // [ '문래', '대림', '구로디지털단지', '신대방', '신림' ]
 
-const it1 = routes[Symbol.iterator]();
-console.log(it1.next()); // { value: '문래', done: false }
-console.log(it1.next()); // { value: '신림', done: false }
-console.log(it1.next()); // { value: undefined, done: true }
-console.log(it1.next()); // { value: undefined, done: true }
+// const it1 = routes[Symbol.iterator]();
+// console.log(it1.next()); // { value: '문래', done: false }
+// console.log(it1.next()); // { value: '신림', done: false }
+// console.log(it1.next()); // { value: undefined, done: true }
+// console.log(it1.next()); // { value: undefined, done: true }
 
-const routes2 = new Subway("구로디지털단지", "성수"); // 32개 정거장
-console.log([...routes]); // ['구로디지털단지', '신대방', ..., '성수']
-const it2 = routes2[Symbol.iterator]();
-while (true) {
-  const x = it2.next();
-  console.log(x);
-  if (x.done) break;
-}
-const route3 = new Subway("문래", "합정"); // 46개 정거장
-console.log("route3>>", [...route3]);
-const route4 = new Subway("신도림", "을지로입구"); // 48개 정거장
-console.log("route4>>", [...route4]);
+// const routes2 = new Subway("구로디지털단지", "성수"); // 32개 정거장
+// console.log([...routes]); // ['구로디지털단지', '신대방', ..., '성수']
+// const it2 = routes2[Symbol.iterator]();
+// while (true) {
+//   const x = it2.next();
+//   console.log(x);
+//   if (x.done) break;
+// }
+// const route3 = new Subway("문래", "합정"); // 46개 정거장
+// console.log("route3>>", [...route3]);
+// const route4 = new Subway("신도림", "을지로입구"); // 48개 정거장
+// console.log("route4>>", [...route4]);
