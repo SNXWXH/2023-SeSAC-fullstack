6432f6aadb79fbe75fec85c3518b3e8b83a786e4
SNXWXH <snxwxh@gmail.com>
2023-09-04 23:28:13 +0900

    nest api 실습

--- nest/nestbook/src/app.module.ts
+++ nest/nestbook/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { UsersController } from './users/users.controller';
 import { BaseService } from './services/base-service';
 import { ServiceA } from './services/a.service';
 import { ServiceB } from './services/b.service';
+
+import { ApiController } from './api/api.controller';
+import { UsersModule } from './users/users.module';
 @Module({
-  controllers: [AppController],
-  providers: [AppService, BaseService, ServiceA, ServiceB],
-  imports: [],
+  controllers: [ApiController, AppController],
+  providers: [ApiController, AppService, BaseService, ServiceA, ServiceB],
+  imports: [UsersModule],
 })
 export class AppModule {}
