8a4a53d498f5ef486d726939667cb3c30b17a799
SNXWXH <snxwxh@gmail.com>
2023-09-08 21:03:01 +0900

    chapter05 실습

--- nest/nestbook/src/app.module.ts
+++ nest/nestbook/src/app.module.ts
@@ -6,21 +6,10 @@ import { ServiceA } from './services/a.service';
 import { ServiceB } from './services/b.service';
 
 import { ApiController } from './api/api.controller';
-// import { UsersModule } from './users/users.module';
-import { EmailService } from './email/email.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 @Module({
-  controllers: [ApiController, AppController, UsersController],
-  providers: [
-    ApiController,
-    AppService,
-    BaseService,
-    ServiceA,
-    ServiceB,
-    EmailService,
-    UsersService,
-  ],
-  imports: [],
+  controllers: [ApiController, AppController],
+  providers: [ApiController, AppService, BaseService, ServiceA, ServiceB],
+  imports: [UsersModule],
 })
 export class AppModule {}
