504a6b865e67c323af58bad940b5f464dd4c194f
SNXWXH <snxwxh@gmail.com>
2023-09-09 09:59:24 +0900

    chapter 06 실습

--- nest/nestbook/src/app.controller.ts
+++ nest/nestbook/src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ServiceB } from './services/b.service';
+import { Request } from 'express';
+// import { CreateUserDto } from './users/dto/create-user.dto';
+// import { log } from 'console';
 
+//안에 내부적으로 injectable이 들어가있음
 @Controller()
 export class AppController {
   greet: string;
@@ -15,10 +19,56 @@ export class AppController {
     return this.serviceB.getHello();
   }
 
+  // @Get('/sayHello')
+  // sayHello(@Query('nickname') nickname: string): string {
+  //   return this.appService.sayHello(nickname);
+  // }
+
+  // // @Get('/sayHello')
+  // // @Redirect('/sayHello')
+  // @Redirect()
+  // @Get('redirect-hello')
+  // redirectHello(@Req() req: Request) {
+  //   console.log('reirectHello.req>>', req.query);
+  //   const { nickname } = req.query;
+  //   return {
+  //     url: '/sayHello?nickname=' + nickname,
+  //   };
+  // }
+
+  @Get('/sayHello')
+  sayHello(@Query('nickname') nickname: string): string {
+    return this.appService.sayHello(nickname);
+  }
+
+  @Get('/sayHelloR')
+  sayHelloR(@Req() req: Request): string {
+    return this.appService.sayHello(JSON.stringify(req.query));
+  }
+
+  // @Get('/sayHello')
+  // @Redirect('/sayHello')
+  @Redirect()
+  @Get('redirect-hello')
+  redirectHello(@Req() req: Request) {
+    console.log('reirectHello.req>>', req.query);
+    // const { query } = req.query;
+    return {
+      url:
+        '/sayHelloR?' + Object.entries(req.query).map(([k, v]) => `${k}=${v}`),
+    };
+  }
+
+  // @Post()
+  // createUser(@Body() createUserDto: CreateUserDto) {
+  //   console.log('🚀  createUserDto:', createUserDto);
+  // }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
   @Post()
   setHello() {
     this.greet = 'xxx';
--- nest/nestbook/src/app.module.ts
+++ nest/nestbook/src/app.module.ts
@@ -7,9 +7,20 @@ import { ServiceB } from './services/b.service';
 
 import { ApiController } from './api/api.controller';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 @Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
   controllers: [ApiController, AppController],
   providers: [ApiController, AppService, BaseService, ServiceA, ServiceB],
-  imports: [UsersModule],
 })
 export class AppModule {}
