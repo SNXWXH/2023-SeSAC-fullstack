ae8995b9eca9ce596bc7a2e09b1a5cff7e363586
SNXWXH <snxwxh@gmail.com>
2023-09-12 14:00:33 +0900

    chapter07 실습

--- nest/nestbook/src/main.ts
+++ nest/nestbook/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //class-transformer가 적용되게 하기
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- nest/nestbook/src/users/dto/create-user.dto.ts
+++ nest/nestbook/src/users/dto/create-user.dto.ts
@@ -1,3 +1,5 @@
+import { BadRequestException, Param } from '@nestjs/common';
+import { Transform } from 'class-transformer';
 import {
   IsEmail,
   IsString,
@@ -5,33 +7,53 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { NotIn } from '../utils/decorators/not-in';
 
 export class CreateUserDto {
   // readonly name: string;
   // readonly email: string;
   // readonly password: string;
+  //* 기본
+  // @Transform((params) =>
+  //   // console.log(params);
+  //   // return params.value;
+  //   params.value.trim(),
+  // )
+  //* passwd와 name이 동일한 문자열 포함 x
+  // @Transform(({ value, obj }) => {
+  //   if (obj.password.includes(obj.name.trim()))
+  //     throw new BadRequestException(
+  //       'password는 name과 같은 문자열을 포함할 수 없습니다',
+  //     );
+  //   return value.trim();
+  // })
 
+  @Transform((params) => params.value.trim())
+  @NotIn('password', {
+    message: 'password는 name과 같은 문자열을 포함할 수 없습니다',
+  })
   @IsString()
   @MinLength(1)
   @MaxLength(20)
-  readonly name: string;
+  name: string;
+  // readonly name: string;
 
   @IsString()
   @IsEmail()
   @MaxLength(60)
-  email: string;
+  readonly email: string;
 
   @IsString()
   //해당 메세지를 나눠서 작성
   //match 두개 짜서 하기
-  // @Matches(/^[A-z\d!@#$%^&*()]{8,30}$/, {
-  //   message: '최소 8자리 이상 30자리 이하, 특수문자를 포함해주세요',
-  // })
-  @Matches(/^[A-z\d!@#$%^&*()]$/, {
-    message: '특수문자를 포함해주세요',
-  })
   @Matches(/^[A-z\d!@#$%^&*()]{8,30}$/, {
-    message: '최소 8자리 이상 30자리 이하입니다',
+    message: '최소 8자리 이상 30자리 이하, 특수문자를 포함해주세요',
   })
+  // @Matches(/^[A-z\d!@#$%^&*()]$/, {
+  //   message: '특수문자를 포함해주세요',
+  // })
+  // @Matches(/^[A-z\d!@#$%^&*()]{8,30}$/, {
+  //   message: '최소 8자리 이상 30자리 이하입니다',
+  // })
   readonly password: string;
 }
